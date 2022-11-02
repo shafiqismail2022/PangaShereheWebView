55ac6409b139fce9fdf6429b2610938c5148ebbc
Shafiq Ismail Soft|Engineer <101216900+shafiqismail2022@users.noreply.github.com>
2022-11-02 16:04:25 +0300

    adding Url

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: WebView(
-        initialUrl: '',
+        initialUrl: 'https://wwww.pangasherehe.com',
         javascriptMode: JavascriptMode.unrestricted,
       ),
     );
