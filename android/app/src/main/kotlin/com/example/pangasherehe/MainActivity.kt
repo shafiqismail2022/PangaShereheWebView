442c85433f22f706a024031733baa61b1b8823d8
Shafiq Ismail Soft|Engineer <101216900+shafiqismail2022@users.noreply.github.com>
2022-11-02 15:54:37 +0300

    Adding a web View of the app

---
+++ android/app/src/main/kotlin/com/example/pangasherehe/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pangasherehe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:io';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebView(
+        initialUrl: '',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
